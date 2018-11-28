f5cae69789581d68561aca754b6505b779ab5c55
hasantha.alahakoon <hasantha.alahakoon@cambio.se>
2018-11-29 00:16:14 +0530

    Following changes done:
    1. Added support to clonning private repos that require ssh keys
    2. Added support to running specific profiles from a test suite

--- Dockerfile
+++ Dockerfile
@@ -17,8 +17,14 @@ RUN apt-get clean
 
 ENV repoUrl=TEST_REPO_URL_NOT_PROVIDED
 
+ENV repoPrivateKeyBase64=BASE_64_ENCODED_REPO_PRIVATE_SSH_KEY
+
+ENV mavenProfile=MAVEN_PROFILE_NOT_SPECIFIED
+
 COPY scripts/run_tests.sh /home/aft/run_tests.sh
 
 COPY mvn/settings.xml /usr/share/maven/conf/settings.xml
+
+RUN mkdir /root/.ssh/
 
 CMD ["/home/aft/run_tests.sh"]
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-/opt/bin/entry_point.sh &
 
-sudo git clone $repoUrl testcode
+echo "${repoPrivateKeyBase64}" | base64 -d > /root/.ssh/id_rsa
+
+chmod 600 /root/.ssh/id_rsa
+
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone $repoUrl testcode
 
 cd testcode
 
-sudo mvn clean install
+mvn clean install -P${mavenProfile}
 
